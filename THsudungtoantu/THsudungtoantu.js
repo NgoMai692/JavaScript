2e8a7276c6c755295812fd7ce2006cd24170e565
unknown <ngomai692@gmail.com>
2021-11-08 22:37:45 +0700

    add 2 file THsudungtoantu.html va THsudungtoantu.js

---
+++ THsudungtoantu/THsudungtoantu.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter the Width")
+inputHeight = prompt("Enter the Height")
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("The erea is: "+ area);
+
